e1f00b52c1a40534db6a098daa68da108223373a
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2021-06-26 00:36:22 +0600

    fixed slow rotation

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,32 +2,47 @@ var btn = document.getElementById('img')
 var angle = 0
 
 anglePerFrame = 28
-function rotate() {
+
+function rotate(dir) {
 
     if (anglePerFrame > 0) {
         btn.style.transform = `rotate(${angle}deg)`
-        angle %= 360; angle += anglePerFrame; anglePerFrame -= 0.1;
-        setTimeout(() => { rotate() }, 20)
+        angle %= 360;
+        dir ? angle += anglePerFrame : angle -= anglePerFrame;
+        anglePerFrame -= 0.1;
+        setTimeout(() => { rotate(dir) }, 20)
     }
     else {
         render(message[getResult()])
+        setTimeout(() => {
+            shouldSlowRotate = 1
+            slowRotaion(Math.floor(Math.random() * 2))
+        }, 2500);
+
     }
 }
 function render(message) {
 
     document.getElementById("message").innerHTML = message
     $('#myModal').modal({ show: true })
+
 }
 function initializeRotate(x) {
     anglePerFrame = x
     angle = 0
-    rotate()
+    rotate(Math.floor(Math.random() * 2))
 }
-rotateInit()
-function rotateInit(){
+slowRotaion(Math.floor(Math.random() * 2))
+var shouldSlowRotate = 1
+
+function slowRotaion(dir) {
     btn.style.transform = `rotate(${angle}deg)`
-    angle %= 360; angle ++;
-    setTimeout(() => { rotateInit() }, 20)
+
+
+    dir ? angle++ : angle--
+    angle += 360
+    angle %= 360;
+    setTimeout(() => { if (shouldSlowRotate) slowRotaion() }, 20)
 }
 function getResult() {
     var pointerAngle = (360 + (angle - 90)) % 360
@@ -39,6 +54,7 @@ function getResult() {
     return cur
 }
 document.getElementById('img').onclick = () => {
+    shouldSlowRotate = 0
     initializeRotate(Math.floor(Math.random() * 20) + 20)
 }
 
