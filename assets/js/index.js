d1fe12f70233a555bb3b5f2b759f901d49306770
urme_15 <nusratjahanurme15@gmail.com>
2021-06-27 15:21:34 +0600

    design updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -36,7 +36,7 @@ function initializeRotate(x) {
     angle = 0
     rotate(Math.floor(Math.random() * 2))
 }
-slowRotaion(Math.floor(Math.random() * 2))
+//slowRotaion(Math.floor(Math.random() * 2))
 var shouldSlowRotate = 1
 
 function slowRotaion(dir) {
